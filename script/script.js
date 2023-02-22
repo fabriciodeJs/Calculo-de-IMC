00914573f92dfe377657707cdc17a8a6f76cd31b
Fabricio de Jesus <114605554+fabriciodeJs@users.noreply.github.com>
2023-02-22 15:49:09 -0300

    visualização do IMC, e tabela IMC

--- script/script.js
+++ script/script.js
@@ -29,37 +29,37 @@ function resultado(imc) {
         b.style.backgroundColor = 'red'
         s.style.color = 'black';
         s.style.fontWeight= 'bold';
-        return s.innerHTML = 'Abaixo do Peso';
+        return s.innerHTML = 'Seu IMC:' + imc +', Abaixo do Peso';
     } else if (imc <= 24.9) {
         b.style.transition = '2s'
         b.style.backgroundColor = 'green'
         s.style.color = 'black';
         s.style.fontWeight= 'bold';
-        return s.innerHTML = 'Peso Normal';
+        return s.innerHTML = 'Seu IMC: ' + imc + ', Peso Normal';
     } else if (imc <= 29.9) {
         b.style.transition = '2s'
         b.style.backgroundColor = 'rgba(28, 212, 77, 0.622)';
         s.style.color = 'black';
         s.style.fontWeight= 'bold';
-        return s.innerHTML = 'Sobrepeso';
+        return s.innerHTML = 'Seu IMC: ' + imc + ', Sobrepeso';
     } else if (imc <= 34.9) {
         b.style.transition = '2s'
         b.style.backgroundColor = 'rgb(202, 99, 99)';
         s.style.color = 'black';
         s.style.fontWeight= 'bold';
-        return s.innerHTML = 'Obsidade Grau 1';
+        return s.innerHTML = 'Seu IMC: ' + imc + ', Obsidade Grau 1';
     } else if (imc <= 39.9) {
         b.style.transition = '2s'
         b.style.backgroundColor = 'rgb(214, 64, 64)';
         s.style.color = 'black';
         s.style.fontWeight= 'bold';
-        return s.innerHTML = 'Obsidade Grau 2';
+        return s.innerHTML = 'Seu IMC: ' + imc + ', Obsidade Grau 2';
     } else {
         b.style.transition = '2s'
         b.style.backgroundColor = 'rgb(244, 0, 0)';
         s.style.color = 'black';
         s.style.fontWeight= 'bold';
-        return s.innerHTML = 'Obsidade Grau 3';
+        return s.innerHTML = 'Seu IMC: ' + imc + ', Obsidade Grau 3';
     }
 
 }
